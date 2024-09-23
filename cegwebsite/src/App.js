1196a046c9aca9e9529ab0e1cedfc36ffe0dfff0
VarshhMohan <vachu2224@gmail.com>
2024-09-23 17:35:09 +0530

    picture+update skeleton

--- cegwebsite/src/App.js
+++ cegwebsite/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import Updates from './Updates';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Updates />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
