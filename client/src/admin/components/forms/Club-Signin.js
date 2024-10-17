304e99b7c099db329da1e71d849196241bbe6a18
Sudeep <sudeep7217@gmail.com>
2024-10-17 18:10:13 +0530

    forms

--- client/src/admin/components/forms/Club-Signin.js
+++ client/src/admin/components/forms/Club-Signin.js
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
 
 const ClubSignin = () => {
-  const [club, setClub] = useState("Select a club");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showDropdown, setShowDropdown] = useState(false);
   const [errors, setErrors] = useState({});
-
-  const clubOptions = ["Football Team", "Basketball Team", "Tennis Team"];
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let formErrors = {};
 
-    if (club === "Select a club") {
-      formErrors.club = "Please select a club";
-    }
     if (!email) {
       formErrors.email = "Email is required";
     }
@@ -27,10 +20,9 @@ const ClubSignin = () => {
     if (Object.keys(formErrors).length > 0) {
       setErrors(formErrors);
     } else {
-      console.log({ club, email, password });
+      console.log({ email, password });
       alert("Form submitted successfully!");
 
-      setClub("Select a club");
       setEmail("");
       setPassword("");
       setErrors({});
@@ -40,7 +32,6 @@ const ClubSignin = () => {
   const handleInputChange = (field, value) => {
     if (field === "email") setEmail(value);
     if (field === "password") setPassword(value);
-    if (field === "club") setClub(value);
 
     setErrors((prevErrors) => ({
       ...prevErrors,
@@ -56,58 +47,6 @@ const ClubSignin = () => {
         </h2>
         <div className="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
           <form onSubmit={handleSubmit}>
-            <div className="mb-5">
-              <label
-                htmlFor="club"
-                className="block mb-2 font-bold text-gray-600"
-              >
-                Select Club
-              </label>
-              <div className="dropdown inline-block relative w-full">
-                <button
-                  type="button"
-                  onClick={() => setShowDropdown(!showDropdown)}
-                  className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center w-full"
-                >
-                  <span className="mr-1">{club}</span>
-                  <svg
-                    className="fill-current h-4 w-4 ml-auto"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </button>
-                {showDropdown && (
-                  <ul className="dropdown-menu absolute bg-white shadow rounded text-gray-700 pt-1 w-full">
-                    {clubOptions.map((option, index) => (
-                      <li key={index}>
-                        <button
-                          type="button"
-                          className={`${
-                            index === 0
-                              ? "rounded-t"
-                              : index === clubOptions.length - 1
-                              ? "rounded-b"
-                              : ""
-                          } bg-gray-200 hover:bg-gray-400 py-2 px-4 w-full text-left`}
-                          onClick={() => {
-                            handleInputChange("club", option);
-                            setShowDropdown(false);
-                          }}
-                        >
-                          {option}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-              {errors.club && (
-                <p className="text-sm text-red-400 mt-2">{errors.club}</p>
-              )}
-            </div>
-
             <div className="mb-5">
               <label
                 htmlFor="email"
