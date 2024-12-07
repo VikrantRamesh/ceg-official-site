82ed014a4ce9dd80bbce6437e35c1675d25bed7a
Tharun R  S <rstharun11@gmail.com>
2024-12-07 18:42:01 +0530

    updated db

--- server/config/ceg_website.sql
+++ server/config/ceg_website.sql
@@ -14,13 +14,18 @@ CREATE TABLE `clubs` (
   `id` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
   `clubname` varchar(50) NOT NULL,
-  `description` varchar(1024) NOT NULL,
-  `socials` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`socials`)),
-  `logo_path` varchar(512) NOT NULL,
-  `banner_path` varchar(512) NOT NULL,
-  `website` varchar(2038) NOT NULL,
-  `members` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`members`))
+  `emoji` char(1) DEFAULT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `socials` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `logo_path` varchar(512) DEFAULT NULL,
+  `banner_path` varchar(512) DEFAULT NULL,
+  `website` varchar(2038) DEFAULT NULL,
+  `members` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `clubs` (`id`, `userid`, `clubname`, `emoji`, `description`, `socials`, `logo_path`, `banner_path`, `website`, `members`) VALUES
+(1, 9, 'ACM CEG Chapter', '💻', 'The ACM-CEG Student Chapter, initiated in 2004, aims to instill an unwavering enthusiasm for computer science in students. The club provides a plethora of networking opportunities and helps to seek advice from the top experts in the field. The club has been steadily working to inculcate an unalloyed interest in Computer Science in students and consequently, stimulating the advancement of computer science as a whole.', '{\"website\":\"https://auceg.acm.org/\",\"instagram\":\"\",\"facebook\":\"acm_ceg\",\"twitter\":\"\",\"linkedin\":\"\",\"youtube\":\"\"}', '\\uploads\\acmceg-logo.png', NULL, 'https://auceg.acm.org/', '[{\"name\":\"Ansh 💣\",\"role\":\"Chair Person\"},{\"name\":\"Shiyam Ganesh\",\"role\":\"Vice Chair Person\"},{\"name\":\"Varsha M\",\"role\":\"Content director\"}]'),
+(2, 9, 'hello', '💻', 'The ACM-CEG Student Chapter, initiated in 2004, aims to instill an unwavering enthusiasm for computer science in students. The club provides a plethora of networking opportunities and helps to seek advice from the top experts in the field. The club has been steadily working to inculcate an unalloyed interest in Computer Science in students and consequently, stimulating the advancement of computer science as a whole.', '{\"website\":\"https://auceg.acm.org/\",\"instagram\":\"\",\"facebook\":\"acm_ceg\",\"twitter\":\"\",\"linkedin\":\"\",\"youtube\":\"\"}', '\\uploads\\acmceg-logo.png', NULL, 'https://auceg.acm.org/', '[{\"name\":\"Ansh 💣\",\"role\":\"Chair Person\"},{\"name\":\"Shiyam Ganesh\",\"role\":\"Vice Chair Person\"},{\"name\":\"Varsha M\",\"role\":\"Content director\"}]');
 
 CREATE TABLE `club_events` (
   `id` int(11) NOT NULL,
@@ -63,10 +68,12 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `users` (`id`, `username`, `password`, `type`, `created_on`) VALUES
-(2, 'admin', '$2a$12$bUYgCrv9m9kG9oI0YHVDsu5.2AzVe6meqst8nM/xMvpBh9K1Axwgq', 'admin', '2024-10-09 13:55:54');
+(2, 'admin', '$2a$12$bUYgCrv9m9kG9oI0YHVDsu5.2AzVe6meqst8nM/xMvpBh9K1Axwgq', 'admin', '2024-10-09 13:55:54'),
+(9, 'acmceg', '$2a$12$.xY/6O7CqZyZbX1BgmEkIeO92NB11O2XBpC9/g41fLKVEdv0C5Cfu', 'club', '2024-10-15 10:41:14');
 
 
 ALTER TABLE `clubs`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `userid_fk` (`userid`);
 
 ALTER TABLE `club_events`
@@ -79,11 +86,14 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 
+ALTER TABLE `clubs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 ALTER TABLE `updates`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 
 ALTER TABLE `clubs`
